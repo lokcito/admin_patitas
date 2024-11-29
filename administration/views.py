bac3a64abcf772e9d478c2830cc6e47337c59033
ray <enciso.leo@gmail.com>
2024-11-28 17:11:21 -0800

    Extendiendo django admin

--- administration/views.py
+++ administration/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .models import Provider, Customer
 
 # Create your views here.
+def v_index(request):
+    context = {
+        "n_providers": 0,
+        "n_customers": 0
+    }
+
+    context["n_providers"] = Provider.objects.all().count()
+    context["n_customers"] = Customer.objects.all().count()
+
+    return render(request, "administration/index.html", 
+                  context)
+
+def v_providers(request):
+    context = {
+        "provs": Provider.objects.all()
+    }
+    return render(request, "administration/providers.html", context)
