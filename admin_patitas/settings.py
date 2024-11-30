b54c91c0d08e3bd3ec35e6dbd877f45cc2e8570e
ray <enciso.leo@gmail.com>
2024-11-30 00:16:02 +0000

    allow

--- admin_patitas/settings.py
+++ admin_patitas/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-e1ch4ibex+_8udnei929yv4v_49m)wr+y!exrkh_@akv*jh=s3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
