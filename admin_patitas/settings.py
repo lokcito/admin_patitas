2b688228cd5e29152975f2bbe426f6b14b0608fa
ray <enciso.leo@gmail.com>
2024-12-02 16:19:15 -0800

    update settings

--- admin_patitas/settings.py
+++ admin_patitas/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e1ch4ibex+_8udnei929yv4v_49m)wr+y!exrkh_@akv*jh=s3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
